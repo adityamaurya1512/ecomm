04438c78f8dfcfe33ebcfe80a0c2717e5b1f5d36
adityamaurya1512 <adityamaurya.ece18@itbhu.ac.in>
2024-08-18 17:19:05 +0530

    files added

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -14,37 +14,33 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ name, price, imgSrc, stock, productId, email }: ProductCardProps) => {
-  const { addToCart, isInCart,items } = useCartStore();
+  const { addToCart, isInCart, items } = useCartStore();
   const [inCart, setInCart] = useState(false);
-  
- 
+
   useEffect(() => {
     setInCart(isInCart(productId, email));
-    
-  },[items, productId, email, isInCart]);
-  
+  }, [items, productId, email, isInCart]);
 
   const handleAddToCart = () => {
     if (email) {
       const image = imgSrc;
-     addToCart({ productId, name, image, price, stock, quantity: 1, email });
+      addToCart({ productId, name, image, price, stock, quantity: 1, email });
     }
-   setInCart(true)
+    setInCart(true);
   };
-  
 
   return (
-    <div className="w-[250px] h-[350px] bg-white shadow-md rounded-lg overflow-hidden flex flex-col justify-between p-4">
-      <div className="flex justify-center">
-        <Image src={imgSrc} alt={name} height={200} width={200} className="object-cover" />
+    <div className="w-[300px] h-[400px] bg-white shadow-md rounded-lg overflow-hidden flex flex-col p-4">
+      <div className="flex justify-center mb-4">
+        <Image src={imgSrc} alt={name} height={180} width={250} className="object-cover" />
       </div>
-      <div className="text-center">
+      <div className="text-center mb-4 flex-grow">
         <h2 className="text-lg font-semibold text-gray-700">{name}</h2>
         <p className="text-gray-500">${price}</p>
       </div>
-      <div className="flex justify-center mt-4">
+      <div className="flex justify-center">
         <button
-          className={`text-white font-semibold py-2 px-4 rounded transition-colors duration-200 flex items-center justify-center ${inCart ? 'bg-gray-500' : 'bg-blue-500 hover:bg-blue-600'}`}
+          className={`text-white font-semibold py-2 px-4 rounded transition-colors duration-200 flex items-center justify-center ${inCart ? 'bg-gray-500 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'}`}
           disabled={inCart}
           onClick={handleAddToCart}
         >
